7ead935183f93383fe0892f55fcbebb8d4fdd0dc
Lê Đô <ledo92@gmail.com>
2026-07-09 08:38:54 +0700

    Cleanup Build012 package folder

--- VovinamERP_Build012_PersonLifecycle/backend/src/VovinamERP.Domain/Instructors/InstructorStatus.cs
+++
@@ -1,8 +0,0 @@
-namespace VovinamERP.Domain.Instructors;
-
-public enum InstructorStatus
-{
-    Active = 1,
-    Inactive = 2,
-    Archived = 3
-}
--- VovinamERP_Build012_PersonLifecycle/backend/src/VovinamERP.Domain/Persons/Gender.cs
+++
@@ -1,9 +0,0 @@
-namespace VovinamERP.Domain.Persons;
-
-public enum Gender
-{
-    Unknown = 0,
-    Male = 1,
-    Female = 2,
-    Other = 3
-}
